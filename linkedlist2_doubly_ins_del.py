488e2a0bce1b277e4347e913ab3fcbdc7174ac0d
Lohith Bollineni <bollineni.lohith9@gmail.com>
2022-09-14 14:08:34 -0400

    doubly

--- linkedlist2_doubly_ins_del.py
+++ linkedlist2_doubly_ins_del.py
@@ -26,7 +26,7 @@ class DLL:
             while a.next is not None:
                 a  = a.next
             while a is not None:
-                print(a.data, end="")
+                print(a.data, end=" ")
                 a = a.prev
 
     def insert_start(self,data):
@@ -36,6 +36,55 @@ class DLL:
         a.prev = ns
         ns.next = a
         self.head = ns
+
+    def insert_end(self,data):
+        print()
+        ne = Node(data)
+        a = self.head
+        while a.next is not None:
+            a = a.next
+        a.next = ne
+        ne.prev = a
+
+    def insert_between(self,position,data):
+        print()
+        nib = Node(data)
+        a =self.head
+        for i in range(1, position-1):
+            a = a.next
+        nib.prev = a
+        nib.next = a.next
+        a.next.prev = nib
+        a.next = nib 
+
+    def delete_start(self):
+        print()
+        a = self.head
+        self.head = a.next
+        a.next = None
+        self.head.prev = None
+
+    def delete_end(self):
+        print()
+        prev = self.head
+        a = self.head.next
+        while a.next is not None:
+            a = a.next
+            prev = prev.next
+        prev.next = None
+        a.prev = None
+
+    def delete_between(self,position):
+        print()
+        a = self.head.next
+        b = self.head
+        for i in range(1 , position-1):
+            a = a.next
+            b = b.next
+        b.next = a.next
+        a.next.prev = b
+        a.next = None
+        a.prev = None
 
 dll = DLL()
 node1 = Node(5)
@@ -49,5 +98,18 @@ node3.next = node4
 dll.for_traverse()
 dll.bac_traverse()
 dll.insert_start(2)
+dll.for_traverse()
+dll.bac_traverse()
+dll.insert_end(28)
+dll.for_traverse()
+dll.insert_between(3,17)
+dll.for_traverse()
+dll.delete_start()
+dll.for_traverse()
+dll.bac_traverse()
+dll.delete_end()
+dll.for_traverse()
+dll.bac_traverse()
+dll.delete_between(3)
 dll.for_traverse()
 dll.bac_traverse()
